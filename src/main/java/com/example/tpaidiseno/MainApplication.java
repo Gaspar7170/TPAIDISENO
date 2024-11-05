b3973acb4a509236e8c45b436eb7e5950b7887dd
JonatanSoruco <sorucojonatan@gmail.com>
2024-11-05 11:49:58 -0300

    aplicando cambios de interfaz

---
+++ src/main/java/com/example/tpaidiseno/MainApplication.java
@@ -0,0 +1,27 @@
+package com.example.tpaidiseno;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
+            primaryStage.setTitle("Pantalla Principal");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/example/tpaidiseno/Pantallas/MainController.java
@@ -0,0 +1,82 @@
+package com.example.tpaidiseno.Pantallas;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.event.ActionEvent;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Objects;
+
+public class MainController {
+    @FXML
+    private GridPane showGrid;
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Button caso5CUButton;
+
+    @FXML
+    private Button casoAlternativo1Button;
+
+    @FXML
+    private ImageView profile;
+
+    @FXML
+    private void initialize() {
+        // Inicialización de componentes
+
+        try {
+            profile = new ImageView();
+            InputStream inputStream = getClass().getResourceAsStream("/avatar.png");
+
+            if (inputStream == null) {
+                System.err.println("No se pudo encontrar el archivo: avatar.png");
+                return; // O maneja el error de otra forma
+            }
+
+            profile.setImage(new Image(inputStream));
+            profile.setFitWidth(100); // Ajusta el ancho
+            profile.setFitHeight(100); // Ajusta la altura
+            // Inicializar otras imágenes y componentes
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleHomeButton(ActionEvent event) {
+        showGrid.getChildren().clear();
+    }
+
+    @FXML
+        private void handleImportarActualizacion(ActionEvent event) {
+        habilitarPantalla();
+    }
+
+    private void habilitarPantalla() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("pantalla-importar.fxml"));
+            showGrid.getChildren().clear();
+            showGrid.getChildren().add(loader.load());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleCasoAlternativo1(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("pantalla-alternativa1.fxml"));
+            showGrid.getChildren().clear();
+            showGrid.getChildren().add(loader.load());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
